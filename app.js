9a1342fd3d53ff1e533871d661dad932dd063e93
Vinith karthik P <vinithkarthik.p@publicissapient.com>
2024-05-01 11:12:51 +0530

    initiated type script

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log('hiiiiii');
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["WRITE_ONLY"] = 2] = "WRITE_ONLY";
+})(Role || (Role = {}));
+;
+var person = {
+    name: 'vinith',
+    age: 30,
+    hobbies: ['football', 'reading'],
+    registrationDate: [12, 'november'],
+    role: Role.ADMIN
+};
+console.log('==role', person.role);
--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
 console.log('hiiiiii');
+
+enum Role { ADMIN, READ_ONLY, WRITE_ONLY };
+
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  registrationDate: [number, string];
+  role: Role;
+} = {
+  name: 'vinith',
+  age: 30,
+  hobbies: ['football', 'reading'],
+  registrationDate: [12, 'november'],
+  role: Role.ADMIN
+}
+
+console.log('==role', person.role);
